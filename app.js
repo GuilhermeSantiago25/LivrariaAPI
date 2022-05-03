88520ff8b75296f9012183dc0a60b03038865e07
NiltonCortesJr <niltoncjr@gmail.com>
2022-05-03 01:46:45 -0300

    Correção dos nomes dos arquivos, estruturação das pastas no padrão definido pelo grupo. Aguardando as atualizações das branches secundárias.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import cors from "cors"
+
+import Clientes from "./src/controllers/Clientes.js";
 import Informatica from "./src/controllers/Informatica.js";
 import Livros from "./src/controllers/Livros.js";
-import cors from "cors"
+import Papelaria from "./src/controllers/Papelaria.js";
+
+import DBMClientes from "./src/DAO/DBMClientes.js"
+import DBMInformatica from "./src/DAO/DBMInformatica.js"
+import DBMLivros from "./src/DAO/DBMLivros.js"
+import DBMPapelaria from "./src/DAO/DBMPapelaria.js"
 
 
 dotenv.config()
@@ -13,9 +21,16 @@ const port = process.env.PORT || 3001
 app.use(cors())
 app.use(express.json())
 
+DBMClientes.createTable();
+DBMInformatica.createTable();
+DBMLivros.createTable();
+DBMPapelaria.createTable();
+
 app.listen(port, ()=>{
     console.log(`Servidor em http://localhost:${port}`)
 })
 
-
-Usuarios.routers(app)
+Clientes.routers(app)
+Informatica.routers(app)
+Livros.routers(app)
+Papelaria.routers(app)
