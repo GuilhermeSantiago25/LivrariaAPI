50df112fe10ddadb5747cda026ae82757c675cfa
NiltonCortesJr <niltoncjr@gmail.com>
2022-05-03 02:06:29 -0300

    Correção das pontuações em app.js.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 
 import Clientes from "./src/controllers/Clientes.js";
 import Informatica from "./src/controllers/Informatica.js";
 import Livros from "./src/controllers/Livros.js";
 import Papelaria from "./src/controllers/Papelaria.js";
 
-import DBMClientes from "./src/DAO/DBMClientes.js"
-import DBMInformatica from "./src/DAO/DBMInformatica.js"
-import DBMLivros from "./src/DAO/DBMLivros.js"
-import DBMPapelaria from "./src/DAO/DBMPapelaria.js"
+import DBMClientes from "./src/DAO/DBMClientes.js";
+import DBMInformatica from "./src/DAO/DBMInformatica.js";
+import DBMLivros from "./src/DAO/DBMLivros.js";
+import DBMPapelaria from "./src/DAO/DBMPapelaria.js";
 
 
 dotenv.config()
@@ -30,7 +30,7 @@ app.listen(port, ()=>{
     console.log(`Servidor em http://localhost:${port}`)
 })
 
-Clientes.routers(app)
-Informatica.routers(app)
-Livros.routers(app)
+Clientes.routers(app);
+Informatica.routers(app);
+Livros.routers(app);
 Papelaria.routers(app)
