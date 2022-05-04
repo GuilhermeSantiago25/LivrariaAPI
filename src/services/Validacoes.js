5adbba77b99071fca480899ed2a0114e0804c636
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-05-04 10:11:27 -0300

    Update files

--- src/services/Validacoes.js
+++ src/services/Validacoes.js
@@ -11,24 +11,6 @@ class Validacoes{
             return false
         }
     }
-    
-    static validaCPF(cpf){
-        const cpfNum = parseInt(cpf)
-        if(cpf.length == 11 && cpfNum == cpf){
-            return true
-        } else {
-            return false
-        }
-    }
-
-    static validaTelefone(telefone){
-        const telefoneNum = parseInt(telefone)
-        if(telefone.length == 11 && telefoneNum == telefone){
-            return true
-        } else {
-            return false
-        }
-    }
 }
 
 export default Validacoes;
--- src/test/Validacoes.test.js
+++ src/test/Validacoes.test.js
@@ -3,25 +3,3 @@ import Validacoes from "../services/Validacoes.js";
 test("Validar se o nome tem pelo menos 3 caracteres.", ()=>{
     expect(Validacoes.validaNome("Ana")).toBe(true)
 });
-
-
-test("Validar se o número de telefone tem 11 digitos", ()=>{
-    expect(Validacoes.validaTelefone("12345678910")).toBe(true)
-});
-test("Validar se o número de telefone tem 11 digitos", ()=>{
-    expect(Validacoes.validaTelefone("1234a567891")).toBe(false)
-});
-test("Validar se o número de telefone tem 11 digitos", ()=>{
-    expect(Validacoes.validaTelefone("1234567891")).toBe(false)
-});
-
-
-test("Validar se o número do cpf tem 11 digitos", ()=>{
-    expect(Validacoes.validaCPF("12345678910")).toBe(true)
-});
-test("Validar se o número de telefone tem 11 digitos", ()=>{
-    expect(Validacoes.validaCPF("1234a567891")).toBe(false)
-});
-test("Validar se o número de telefone tem 11 digitos", ()=>{
-    expect(Validacoes.validaCPF("1234567891")).toBe(false)
-});
