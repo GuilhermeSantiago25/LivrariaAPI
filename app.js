6ddd21ca39b5d0bd8d9f6af97c837401a7299ade
Izadora Ferreira <izadorasantos20@hotmail.com>
2022-05-04 14:38:58 -0300

    atualização Livro

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import Informatica from "./src/controllers/Informatica.js";
 import Livros from "./src/controllers/Livros.js";
-import cors from "cors"
+import DBMLivros from "./src/DAO/DBMLivros.js";
+import cors from "cors";
+import fs from "fs";
+import https from "https";
+
+
 
 
 dotenv.config()
@@ -13,9 +17,16 @@ const port = process.env.PORT || 3001
 app.use(cors())
 app.use(express.json())
 
+DBMLivros.createTable()
+
+
 app.listen(port, ()=>{
     console.log(`Servidor em http://localhost:${port}`)
 })
 
+https.createServer({
+    cert: fs.readFileSync("src/utils/code.crt"),
+    key: fs.readFileSync("src/utils/code.key")
+}, app).listen(3003, () => console.log("Servidor rodando também em https://localhost:3003"))
 
-Usuarios.routers(app)
+Livros.routers(app)
--- src/controllers/Livros.js
+++ src/controllers/Livros.js
@@ -1,19 +1,38 @@
-//import DatabaseMetodos from "../DAO/DatabaseMetodos.js"
-//import UsuariosModel from "../models/UsuariosModel.js"
-//import Validacoes from "../services/Validacoes.js"
+import DBMLivros from "../DAO/DBMLivros.js"
+import LivrosModel from "../models/LivrosModel.js"
+import Validacoes from "../services/Validacoes.js"
 
 class Livros{
     static routers(app){
         
-        app.get("/usuarios", async (req, res) =>{
-            const response = await DatabaseMetodos.listarTodos()
+        app.get("/Livros", async (req, res) =>{
+            const response = await DBMLivros.listarTodos()
             res.status(200).json(response)
         })
-        app.post("/usuarios", async(req, res) => {
+
+        app.get("/Livros/:id", async (req, res) =>{
+            const response = await DBMLivros.listaPorId(req.body.id)
+            res.status(200).json(response)
+        })
+
+        app.put('/Livros/:id', async(req, res) => {
             try {                
-                if(Validacoes.validaNome(req.body.nome)){
-                    const usuario = new UsuariosModel(...Object.values(req.body))
-                    const response = await DatabaseMetodos.popular(usuario)
+                if(Validacoes.validaNome(req.body.nome) && Validacoes.validaValor(req.body.valor)){
+                    const atualizarLivro = await DBMLivros.atualizaPorId(req.body, req.body.id)
+                    res.status(200).json(atualizarLivro);
+                }else {
+                    throw new Error("Requisição fora dos padrões, favor rever.")
+                }
+            } catch (e) {
+                res.status(400).json({erro: e.message})
+            }
+        });
+
+        app.post("/Livros", async(req, res) => {
+            try {                
+                if(Validacoes.validaNome(req.body.nome)&& Validacoes.validaValor(req.body.valor))  {
+                    const inserirLivro = new LivrosModel(...Object.values(req.body))
+                    const response = await DBMLivros.popular(inserirLivro)
                     res.status(201).json(response)
                 } else {
                     throw new Error("Requisição fora dos padrões, favor rever.")
@@ -21,7 +40,12 @@ class Livros{
             } catch (e) {
                 res.status(400).json({erro: e.message})
             }
-        })
+        });
+
+        app.delete('/Livros/:id', async(req, res) => {               
+                    const deletarLivro = await DBMLivros.deletaPorId(req.body.id)
+                    res.status(201).json(deletarLivro)
+        });
     }
 }
 
