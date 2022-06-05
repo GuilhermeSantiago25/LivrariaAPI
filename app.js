fdac57d532214d1934e0cac8eeadeff8f0057062
Vítor Del' Duca <vitordelduca@hotmail.com>
2022-06-04 22:23:58 -0300

    fix: alteração do caminho das importações
    fix: alteração do caminho das importações

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import Clientes from "./src/controllers/Clientes.js";
-import Informatica from "./src/controllers/Informatica.js";
-import Livros from "./src/controllers/Livros.js";
-import Papelaria from "./src/controllers/Papelaria.js";
+import Clientes from "/src/controllers/Clientes.js";
+import Informatica from "/src/controllers/Informatica.js";
+import Livros from "/src/controllers/Livros.js";
+import Papelaria from "/src/controllers/Papelaria.js";
 
-import DBMClientes from "./src/DAO/DBMClientes.js";
-import DBMInformatica from "./src/DAO/DBMInformatica.js";
-import DBMLivros from "./src/DAO/DBMLivros.js";
-import DBMPapelaria from "./src/DAO/DBMPapelaria.js";
+import DBMClientes from "/src/DAO/DBMClientes.js";
+import DBMInformatica from "/src/DAO/DBMInformatica.js";
+import DBMLivros from "/src/DAO/DBMLivros.js";
+import DBMPapelaria from "/src/DAO/DBMPapelaria.js";
 
 dotenv.config()
 
